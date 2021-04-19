f75e0d42aef93effb73cf340bf170a17b13ecdfc
Wowlfy <maxime.giraudeau@hotmail.fr>
2021-04-19 10:29:41 +0200

    forms avancé 1.2

--- form.php
+++ form.php
@@ -1,46 +1,4 @@
-
-<form action="" method="POST" novalidate>
-<?php
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = array_map('trim', $_POST);
-
-    $errors = [];
-
-    if (empty($data['user_firstname'])) {
-        $errors[] = 'Le prénom est obligatoire';
-    }
-    if (empty($data['user_name'])) {
-        $errors[] = '⚠Le nom est obligatoire';
-    }
-    if (empty($data['user_email'])) {
-        $errors[] = '⚠L\'Email est obligatoire';
-    }
-    if (!filter_var($data['user_email'], FILTER_VALIDATE_EMAIL)) {
-        $errors[] = '⚠Mauvais format d\'email';
-    }
-    if (empty($data['user_message'])) {
-        $errors[] = 'Un message est obligatoire';
-    }
-    if (empty($data['user_sujet'])) {
-        $errors[] = 'Un sujet est obligatoire';
-    }
-    if (empty($data['user_telephone'])) {
-        $errors[] = 'le numero de telephone est obligatoire';
-    }
-    if (empty($errors)) {
-        header('Location: thanks.php');
-    }
-}
-?>
-
-<?php if (!empty($errors)) : ?>
-            <ul class="error">
-                <?php foreach ($errors as $error) : ?>
-                    <li><?= $error ?></li>
-                <?php endforeach; ?>
-            </ul>
-        <?php endif; ?>
+<form action="thanks.php" method="POST">
     <div>
         <label for="nom">Nom :</label>
         <input type="text" id="nom" name="user_name" value="<?= $data['user_name'] ?? '' ?>">
@@ -60,13 +18,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <div>
         <label for="sujet">Sujet :</label>
         <select type="sujet" id="sujet" name="user_sujet" value="<?= $data['user_sujet'] ?? '' ?>">
-        <option value="choisi" selected></option>
-        <option value="covid19">Covid19</option>
-        <option value="covid20">Covid20</option>
-        <option value="illuminati">Illuminati</option>
-        <option value="pyramides">Pyramides</option>
-        <option value="OVNI">OVNI</option>
-        <option value="reptilien">Les reptiliens</option>
+            <option value="choisi" selected></option>
+            <option value="covid19">Covid19</option>
+            <option value="covid20">Covid20</option>
+            <option value="illuminati">Illuminati</option>
+            <option value="pyramides">Pyramides</option>
+            <option value="OVNI">OVNI</option>
+            <option value="reptilien">Les reptiliens</option>
         </select>
     </div>
     <div>
--- thanks.php
+++ thanks.php
@@ -1,5 +1,44 @@
+<?php
+var_dump($_POST);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = array_map('trim', $_POST);
 
-<?php 
+    $errors = [];
+
+    if (empty($data['user_firstname'])) {
+        $errors[] = 'Le prénom est obligatoire';
+    }
+    if (empty($data['user_name'])) {
+        $errors[] = '⚠Le nom est obligatoire';
+    }
+    if (empty($data['user_email'])) {
+        $errors[] = '⚠L\'Email est obligatoire';
+    }
+    if (!filter_var($data['user_email'], FILTER_VALIDATE_EMAIL)) {
+        $errors[] = '⚠Mauvais format d\'email';
+    }
+    if (empty($data['user_message'])) {
+        $errors[] = 'Un message est obligatoire';
+    }
+    if (empty($data['user_sujet'])) {
+        $errors[] = 'Un sujet est obligatoire';
+    }
+    if (empty($data['user_telephone'])) {
+        $errors[] = 'le numero de telephone est obligatoire';
+    }
+    if (empty($errors)) {
+        header('Location: thanks.php');
+    }
+}
+?>
+<?php if (!empty($errors)) : ?>
+    <ul class="error">
+        <?php foreach ($errors as $error) : ?>
+            <li><?= $error ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+<?php
 $firstname = $_POST['user_firstname'];
 $name = $_POST['user_name'];
 $sujet = $_POST['user_sujet'];
@@ -7,8 +46,9 @@ $email = $_POST['user_email'];
 $telephone = $_POST['user_telephone'];
 $message = $_POST['user_message'];
 
-echo "Merci " . $firstname ." ". $name . " de nous avoir contacté à propos de " . $sujet. PHP_EOL;
+echo "Merci " . $firstname . " " . $name . " de nous avoir contacté à propos de " . $sujet . PHP_EOL;
 
 echo "Un de nos conseiller vous contactera soit à l’adresse " . $email . " ou par téléphone au " . $telephone . " dans les plus brefs délais pour traiter votre demande :" . PHP_EOL;
 
 echo $message;
+?>
