71b4b9ebf32613a20c95d7038cd3b734d1a08308
Wowlfy <maxime.giraudeau@hotmail.fr>
2021-04-19 10:34:03 +0200

    forms avancé 1.2

--- thanks.php
+++ thanks.php
@@ -1,5 +1,4 @@
 <?php
-var_dump($_POST);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = array_map('trim', $_POST);
 
@@ -27,7 +26,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $errors[] = 'le numero de telephone est obligatoire';
     }
     if (empty($errors)) {
-        header('Location: thanks.php');
+        $firstname = $_POST['user_firstname'];
+        $name = $_POST['user_name'];
+        $sujet = $_POST['user_sujet'];
+        $email = $_POST['user_email'];
+        $telephone = $_POST['user_telephone'];
+        $message = $_POST['user_message'];
+
+        echo "Merci " . $firstname . " " . $name . " de nous avoir contacté à propos de " . $sujet . PHP_EOL;
+
+        echo "Un de nos conseiller vous contactera soit à l’adresse " . $email . " ou par téléphone au " . $telephone . " dans les plus brefs délais pour traiter votre demande :" . PHP_EOL;
+
+        echo $message;
     }
 }
 ?>
@@ -38,17 +48,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <?php endforeach; ?>
     </ul>
 <?php endif; ?>
-<?php
-$firstname = $_POST['user_firstname'];
-$name = $_POST['user_name'];
-$sujet = $_POST['user_sujet'];
-$email = $_POST['user_email'];
-$telephone = $_POST['user_telephone'];
-$message = $_POST['user_message'];
-
-echo "Merci " . $firstname . " " . $name . " de nous avoir contacté à propos de " . $sujet . PHP_EOL;
-
-echo "Un de nos conseiller vous contactera soit à l’adresse " . $email . " ou par téléphone au " . $telephone . " dans les plus brefs délais pour traiter votre demande :" . PHP_EOL;
-
-echo $message;
-?>
